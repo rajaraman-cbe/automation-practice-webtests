416ce4ae35f273022e9e89a1ac0426e5e62997ad
Rajaraman <rajaramanmgit@outlook.com>
2021-04-07 14:45:32 +0100

    util file updated

--- utils/util.go
+++ utils/util.go
@@ -9,23 +9,22 @@ import (
 
 func GenerateRandomString(length int) string {
 	rand.Seed(time.Now().UnixNano())
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" +
-		"abcdefghijklmnopqrstuvwxyz" +
-		"0123456789")
-	var b strings.Builder
-	for i := 0; i < length; i++ {
-		b.WriteRune(chars[rand.Intn(len(chars))])
-	}
-	return b.String()
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz")
+	return strBuilder(chars,length).String()
 }
 
 func GenerateRandomNumber(length int) int64 {
 	rand.Seed(time.Now().UnixNano())
 	chars := []rune("0123456789")
+	num, _:=strconv.ParseInt(strBuilder(chars,length).String(),10,64)
+	return num
+}
+
+func strBuilder(chars []rune, length int) *strings.Builder {
 	var b strings.Builder
 	for i := 0; i < length; i++ {
 		b.WriteRune(chars[rand.Intn(len(chars))])
 	}
-	num, _:=strconv.ParseInt(b.String(),10,64)
-	return num
+	return &b
 }
